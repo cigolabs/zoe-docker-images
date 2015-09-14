4075bd9ad0791f156353d629318d7546cdaeec0b
Daniele Venzano <venza@brownhat.org>
2015-09-14 09:45:45 +0000

    Fix pull script

--- swarm_pull.sh
+++ swarm_pull.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-#IMAGE_VER=1.3
-REGISTRY=10.1.0.1:5000
+IMAGE_VER=1.2
+REGISTRY="10.1.0.1:5000"  # Set this to empty to use Docker Hub
+
 IMAGES="zoerepo/spark-master \
         zoerepo/spark-worker \
         zoerepo/spark-submit \
         zoerepo/spark-shell \
-        zoerepo/spark-notebook"
+        zoerepo/spark-scala-notebook"
 SWARM=10.1.0.1:2380
 
 for i in ${IMAGES}; do
     if [ -n "${IMAGE_VER}" ]; then
-        image=${i}:${IMAGE_VER}
+        image=${REGISTRY}/${i}:${IMAGE_VER}
     else
-        image="${i}"
+        image=${REGISTRY}/${i}
     fi
     docker -H ${SWARM} pull ${image}
 done
