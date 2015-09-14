e8e387bddff0c37d169f9aec7da6578989857a67
Daniele Venzano <venza@brownhat.org>
2015-09-14 07:51:57 +0000

    Update to Spark 1.5 and Notebook 0.6.1

--- build_images.sh
+++ build_images.sh
@@ -6,11 +6,17 @@ REPOSITORY=zoerepo
 
 LOCAL_REGISTRY=10.1.0.1:5000
 
+VERSION=`git branch | grep \* | awk '{ print $2 }'`
+
+if [ ${VERSION} == 'master' ]; then
+	VERSION=""
+fi
+
 for d in spark-master spark-worker spark-shell spark-submit spark-scala-notebook; do
   pushd $d
-  docker build -t ${REPOSITORY}/$d .
-  docker tag ${REPOSITORY}/$d ${LOCAL_REGISTRY}/${REPOSITORY}/$d
-  docker push ${LOCAL_REGISTRY}/${REPOSITORY}/$d
+  docker build -t ${REPOSITORY}/$d:${VERSION} .
+  docker tag ${REPOSITORY}/$d:${VERSION} ${LOCAL_REGISTRY}/${REPOSITORY}/$d:${VERSION}
+  docker push ${LOCAL_REGISTRY}/${REPOSITORY}/$d:${VERSION}
   popd
 done
 
