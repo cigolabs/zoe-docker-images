ec1129bf9152e84d936f84672543cc10a69f076c
Daniele Venzano <venza@brownhat.org>
2015-09-14 07:46:22 +0000

    Add version tag

--- build_images.sh
+++ build_images.sh
@@ -8,9 +8,9 @@ LOCAL_REGISTRY=10.1.0.1:5000
 
 for d in spark-master spark-worker spark-shell spark-submit spark-scala-notebook; do
   pushd $d
-  docker build -t ${REPOSITORY}/$d .
-  docker tag ${REPOSITORY}/$d ${LOCAL_REGISTRY}/${REPOSITORY}/$d
-  docker push ${LOCAL_REGISTRY}/${REPOSITORY}/$d
+  docker build -t ${REPOSITORY}/$d:1.2 .
+  docker tag ${REPOSITORY}/$d:1.2 ${LOCAL_REGISTRY}/${REPOSITORY}/$d:1.2
+  docker push ${LOCAL_REGISTRY}/${REPOSITORY}/$d:1.2
   popd
 done
 
