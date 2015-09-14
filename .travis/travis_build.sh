7331fc4dbd7fb05d503fdc6b93f736b53fbcdb76
Daniele Venzano <venza@brownhat.org>
2015-09-14 11:49:03 +0000

    Remove colon from empty version (travis)

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 set -e
+set -x
 
 VERSION=`git branch | grep \* | awk '{ print $2 }'`
 
 if [ ${VERSION} == 'master' ]; then
 	VERSION=""
+else
+	VERSION=":$VERSION"
 fi
 
 for d in spark-master spark-worker spark-shell spark-submit spark-scala-notebook; do
   pushd $d
-  docker build -t $d:${VERSION} .
+  docker build -t $d${VERSION} .
   popd
 done
 
