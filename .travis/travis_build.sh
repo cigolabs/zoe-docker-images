866b439cb8cf6428676ecc5653bc633445f1b689
Daniele Venzano <venza@brownhat.org>
2015-09-14 11:54:15 +0000

    Forget all about tags and versions

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -3,17 +3,9 @@
 set -e
 set -x
 
-VERSION=`git branch | grep \* | awk '{ print $2 }'`
-
-if [ ${VERSION} == 'master' ]; then
-	VERSION=""
-else
-	VERSION=":$VERSION"
-fi
-
 for d in spark-master spark-worker spark-shell spark-submit spark-scala-notebook; do
   pushd $d
-  docker build -t $d${VERSION} .
+  docker build .
   popd
 done
 
