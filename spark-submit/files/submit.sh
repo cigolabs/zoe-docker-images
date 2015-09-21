36c7f364f03e63c4addab590348486c8781256d4
Daniele Venzano <venza@brownhat.org>
2015-09-21 11:41:03 +0000

    Simplify spark submit container env variables

--- spark-submit/files/submit.sh
+++ spark-submit/files/submit.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-if [ -z ${APPLICATION_ID} ]; then
-    echo "No application ID provided, cannot continue"
-    exit 1
-fi
 
-mkdir /tmp/${APPLICATION_ID}
-cd  /tmp/${APPLICATION_ID}
+mkdir /tmp/spark-submit
+cd  /tmp/spark-submit
 
 echo "Downloading application from ${APPLICATION_URL}"
 curl "${APPLICATION_URL}" --retry 5 --retry-delay 2 -o app.zip
 unzip app.zip
 
 /opt/spark/bin/spark-submit --master spark://${SPARK_MASTER_IP}:7077 --executor-memory=${SPARK_EXECUTOR_RAM} ${SPARK_OPTIONS} "$@"
+
