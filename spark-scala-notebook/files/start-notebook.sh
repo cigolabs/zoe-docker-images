77dc45d16413760e3d763beb8dce0df85c88ae96
Daniele Venzano <venza@brownhat.org>
2015-09-22 09:50:58 +0000

    Do not hardcode the proxy base path in notebooks

--- spark-scala-notebook/files/start-notebook.sh
+++ spark-scala-notebook/files/start-notebook.sh
@@ -4,4 +4,4 @@ cd /opt/spark-notebook
 
 cat ../application.conf | sed -e "s/SPARK_MASTER_IP/$SPARK_MASTER_IP/" -e "s/SPARK_EXEC_RAM/$SPARK_EXECUTOR_RAM/" > conf/application.conf
 
-./bin/spark-notebook -Dconfig.file=./conf/application.conf -Dapplication.context=/proxy/$PROXY_ID
+./bin/spark-notebook -Dconfig.file=./conf/application.conf -Dapplication.context=$PROXY_PATH
